9170fd93469a401de7162947c4caa3a11dcaf9c5
Prabesh <prabesh.bista369@gmail.com>
2022-08-11 15:15:39 -0400

    [learned] arthimetic operation in Rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    //variable in Rust
+    //Integer Data-Types
+    let mut x:i8=90;
+    //By default variables are immutable in Rust
+    x=20;
+    println!("X value is {}",x);
+
+    //DataType in Rust
+    //Floating Data Types
+    let  number:f32=20.6555;
+    println!("{}",number);
+
+    //Arthmetic Operaitons
+
+    let a=20;
+    let b=40;
+    let c=(b+1)/a;
+    println!("{}",c);
+
+
+
+    
 }
