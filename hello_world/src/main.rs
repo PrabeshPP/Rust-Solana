944662549674c86a9306874e5594ac8789c6bcfd
Prabesh <prabesh.bista369@gmail.com>
2022-08-12 10:44:21 -0400

    [learned] print formatting

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,24 +1,40 @@
 fn main() {
     //variable in Rust
     //Integer Data-Types
-    let mut x:i8=90;
+    // let mut x:i8=90;
     //By default variables are immutable in Rust
-    x=20;
-    println!("X value is {}",x);
+    // x=20;
+    // println!("X value is {}",x);
 
     //DataType in Rust
     //Floating Data Types
-    let  number:f32=20.6555;
-    println!("{}",number);
+    // let  number:f32=20.6555;
+    // println!("{}",number);
 
     //Arthmetic Operaitons
 
-    let a=20;
-    let b=40;
-    let c=(b+1)/a;
-    println!("{}",c);
+    // let a=20;
+    // let b=40;
+    // let c=(b+1)/a;
+    // println!("{}",c);
 
 
+    //Formating print statements
+    // let i=30.33;
+    // let j=3.3;
+    // let k=i/j;
+    // println!("k is {:.2}",k);
 
+    //Positional Parameters
+
+    // let x=20;
+    // let y=40;
     
+    // let result=y*x;
+    
+    // println!("x is {0} y is {1} and {0} multiply {1} is {2}",x,y,result);
+
+    //BitWise Operations
+    
+
 }
