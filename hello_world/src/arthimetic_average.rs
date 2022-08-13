3365810b653e682ead3d707582476e32b73fabd6
Prabesh <prabesh.bista369@gmail.com>
2022-08-13 07:30:08 -0400

    [completed] artimetic average using rust programming language

---
+++ hello_world/src/arthimetic_average.rs
@@ -0,0 +1,9 @@
+pub fn sum_average(){
+    let a=13;
+    let b=2.3;
+    let c:f32=120.0;
+
+    let average=(a as f64+b +c as f64)/3.0;
+    assert_eq!(average,45.1);
+    println!("Test Passed!");
+}
--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,38 +1,40 @@
+mod arthimetic_average;
+
 fn main() {
     //variable in Rust
     //Integer Data-Types
-    // let mut x:i8=90;
-    //By default variables are immutable in Rust
-    // x=20;
-    // println!("X value is {}",x);
+    let mut x:i8=90;
+    // By default variables are immutable in Rust
+    x=20;
+    println!("X value is {}",x);
 
     //DataType in Rust
     //Floating Data Types
-    // let  number:f32=20.6555;
-    // println!("{}",number);
+    let  number:f32=20.6555;
+    println!("{}",number);
 
     //Arthmetic Operaitons
 
-    // let a=20;
-    // let b=40;
-    // let c=(b+1)/a;
-    // println!("{}",c);
+    let a=20;
+    let b=40;
+    let c=(b+1)/a;
+    println!("{}",c);
 
 
     //Formating print statements
-    // let i=30.33;
-    // let j=3.3;
-    // let k=i/j;
-    // println!("k is {:.2}",k);
+    let i=30.33;
+    let j=3.3;
+    let k=i/j;
+    println!("k is {:.2}",k);
 
     //Positional Parameters
 
-    // let x=20;
-    // let y=40;
+    let x=20;
+    let y=40;
     
-    // let result=y*x;
+    let result=y*x;
     
-    // println!("x is {0} y is {1} and {0} multiply {1} is {2}",x,y,result);
+    println!("x is {0} y is {1} and {0} multiply {1} is {2}",x,y,result);
 
     //BitWise Operations
     //Not
@@ -41,23 +43,23 @@ fn main() {
     //Xor
     //Shift
 
-    // let mut value=10;
-    // value=!value;
-    // print!("value is {}",value)
+    let mut value=10;
+    value=!value;
+    print!("value is {}",value);
     
     // And wise bit operation
-    // let first_bit=2;
-    // let second_bit=3;
-    // println!("And Bit Wise Operation is {}",first_bit&second_bit);
+    let first_bit=2;
+    let second_bit=3;
+    println!("And Bit Wise Operation is {}",first_bit&second_bit);
 
     //Checking whether the given number is odd or even using bitwise operator
-    // let number=100;
-    // let result=number&1;
-    // if result==0 {
-    //     println!("Number is even");
-    // }else {
-    //     println!("Number is Odd");
-    // }
+    let number=100;
+    let result=number&1;
+    if result==0 {
+        println!("Number is even");
+    }else {
+        println!("Number is Odd");
+    }
 
 
     //XOR Operator
@@ -76,8 +78,15 @@ fn main() {
     println!("Value after shifting with Right Shifter {}",random1>>4);
 
 
+    //Characters in Rust
+    let letter='a';
+    let finger='\u{261D}';
+    println!("{letter}");
+    println!("{finger}");
+
+
+    //Average Solution of three numbers
+    arthimetic_average::sum_average();
     
-
-
 
 }
