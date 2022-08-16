84b2003bc6c2b4431ece7f8e4b3f5142bea74ade
Prabesh <prabesh.bista369@gmail.com>
2022-08-16 18:05:04 -0400

    [learned] passing parameters in Rust

--- hello_world/src/functions_rust.rs
+++ hello_world/src/functions_rust.rs
@@ -1,3 +1,4 @@
-pub fn say_hello(){
-    println!("This is just an another function for Rust.");
+pub fn say_hello(number1:i32,number2:i32){
+    let total=number1+number2;
+    println!("Sum of two numbers is {total}");
 }
